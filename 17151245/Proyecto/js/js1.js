087228a8934f2297194b3cdbdfd9e84b223feec7
unknown <adrianharorojas@gmail.com>
2020-03-07 09:15:15 +0900

    Responsive

--- 17151245/Proyecto/js/js1.js
+++ 17151245/Proyecto/js/js1.js
@@ -1,13 +1,8 @@
 function myFunction(imgs) {
-    // Get the expanded image
     var expandImg = document.getElementById("expandedImg");
-    // Get the image text
     var imgTextG = document.getElementById("imgtextG");
-    // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgTextG.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
 
 }
